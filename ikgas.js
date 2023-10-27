539c54fc299f9724a600732ee7852fc98337a8a6
Namdnbk <147038845+Namdnbk@users.noreply.github.com>
2023-10-28 01:56:53 +0700

    Update ikgas.js

--- ikgas.js
+++ ikgas.js
@@ -4,5 +4,3 @@ const ikgasm3 = ikga2[Math.floor(Math.random()*ikga2.length)];
 const ikgasm4 = ikga1[Math.floor(Math.random()*ikga1.length)];
 const ikgasm5 = ikgasa2[Math.floor(Math.random()*ikgasa2.length)];
 const ikgasm6 = ikgasa1[Math.floor(Math.random()*ikgasa1.length)];
-const ikgasm7 = ikgas2[Math.floor(Math.random()*ikgas2.length)];
-const ikgasm8 = ikgas1[Math.floor(Math.random()*ikgas1.length)];
